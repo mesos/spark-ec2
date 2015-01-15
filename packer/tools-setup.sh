5361a8da305056f4a645993e669ffd54a917878f
Nicholas Chammas <nicholas.chammas@gmail.com>
2015-01-15 16:15:14 -0500

    fix GNU parallel install

--- packer/tools-setup.sh
+++ packer/tools-setup.sh
@@ -5,7 +5,7 @@ set -e
 sudo debuginfo-install -q -y kernel
 
 # Both of these can be problematic.
-# sudo yum update -y  # this can be problematic
+# sudo yum update -y
 # sudo yum update -y --security
 
 sudo yum install -y pssh git
@@ -18,8 +18,8 @@ PARALLEL_VERSION="20141122"
 wget "http://ftpmirror.gnu.org/parallel/parallel-${PARALLEL_VERSION}.tar.bz2"
 bzip2 -dc "parallel-${PARALLEL_VERSION}.tar.bz2" | tar xvf -
 pushd "parallel-${PARALLEL_VERSION}"
-sudo ./configure
-sudo make
+./configure --prefix=/usr  # Amazon Linux root user doesn't have /usr/local on its $PATH
+make
 sudo make install
 popd
 rm -rf "./parallel-${PARALLEL_VERSION}*"
