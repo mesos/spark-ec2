7e0acb086db67601e60bdc1c3b932fd1711374de
cheng chang <myairia@gmail.com>
2015-03-05 12:41:20 +0800

    make tachyon version configurable
    make tachyon configurable
    
    merge conflitc
    
    merge

--- tachyon/init.sh
+++ tachyon/init.sh
@@ -7,8 +7,6 @@ if [ -d "tachyon" ]; then
   return 0
 fi
 
-TACHYON_VERSION=0.4.1
-
 # Github tag:
 if [[ "$TACHYON_VERSION" == *\|* ]]
 then
@@ -26,9 +24,30 @@ else
     0.4.1)
       wget https://s3.amazonaws.com/Tachyon/tachyon-0.4.1-bin.tar.gz
       ;;
+    0.5.0)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.5.0-bin.tar.gz
+      else
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.5.0-cdh4-bin.tar.gz
+      fi
+      ;;
+    0.6.0)
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.0-bin.tar.gz
+      else
+        wget https://s3.amazonaws.com/Tachyon/tachyon-0.6.0-cdh4-bin.tar.gz
+      fi
+      ;;
     *)
-      echo "ERROR: Unknown Tachyon version"
-      return -1
+      if [[ "$HADOOP_MAJOR_VERSION" == "1" ]]; then
+        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-bin.tar.gz
+      else
+        wget https://s3.amazonaws.com/Tachyon/tachyon-$TACHYON_VERSION-cdh4-bin.tar.gz
+      fi
+      if [ $? != 0 ]; then
+        echo "ERROR: Unknown Tachyon version"
+        return -1
+      fi
   esac
 
   echo "Unpacking Tachyon"
