388f31b49f4e41a1b639f74ce8272205ab7fd18f
Vladimir Vladimirov <vladimir.vladimirov@magnetic.com>
2015-06-12 11:46:53 -0400

    always try to clean older versions than install pinned ones

--- ganglia/init.sh
+++ ganglia/init.sh
@@ -9,19 +9,18 @@ mkdir -p /mnt/ganglia/rrds
 chown -R nobody:nobody /mnt/ganglia/rrds
 
 
-#Uninstall older version of ganglia if it was reinstalled in AMI
-ssh -t -t $SSH_OPTS root@$node "sudo yum remove -q -y httpd* php* ganglia ganglia-web ganglia-gmond ganglia-gmetad" & sleep 0.3
-
 # Install ganglia
 # TODO: Remove this once the AMI has ganglia by default
 
 GANGLIA_PACKAGES="httpd24-2.4* php56-5.6* ganglia-3.6* ganglia-web-3.5* ganglia-gmond-3.6* ganglia-gmetad-3.6*"
 
-if ! rpm --quiet -q $GANGLIA_PACKAGES; then
-  yum install -q -y $GANGLIA_PACKAGES;
-fi
+#Uninstall older version of ganglia if it was reinstalled in AMI
+yum remove -q -y httpd* php* ganglia ganglia-web ganglia-gmond ganglia-gmetad & sleep 0.3
+yum install -q -y $GANGLIA_PACKAGES
+
 for node in $SLAVES $OTHER_MASTERS; do
-  ssh -t -t $SSH_OPTS root@$node "if ! rpm --quiet -q $GANGLIA_PACKAGES; then yum install -q -y $GANGLIA_PACKAGES; fi" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "yum remove -q -y httpd* php* ganglia ganglia-web ganglia-gmond ganglia-gmetad" & sleep 0.3
+  ssh -t -t $SSH_OPTS root@$node "yum install -q -y $GANGLIA_PACKAGES" & sleep 0.3
 done
 wait
 
